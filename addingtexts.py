1a69c1b640de8aa207dd192961ff2df4c6027e95
Alfonso Lozoya <alfonsolozoya@ymail.com>
2019-05-19 10:00:55 -0500

    add html tags

--- addingtexts.py
+++ addingtexts.py
@@ -1,12 +1,25 @@
 file = open("texts.txt", "r")
+newtexts = ""
 for line in file:
     nob = 0
     for ni in line:
         if(ni == ' '):
             nob += 1
         else:
+            nos = nob
             nob /= 4
-            print("tab"*int(nob))
+            if (nob > 0):
+                line = line.replace("'", "\\'")
+                line = line.replace("`", "\\`")
+                line = line.replace('"', '\\"')
+                newtexts += "+\"<br> "+ "&nbsp; "*int(nob) + line[nos:-1] + "\"\n"
+                #print("+\"<br> "+ "&nbsp; "*int(nob) + line[:-2] + "\"")
+            else:
+                newtexts += '+"<br> '+ line[:-1] + '"\n'
+                #print('+"<br> '+ line[:-2] + '"')
             break
-
+#print(newtexts)
 file.close()
+newfile = open("newtexts.txt", "w")
+newfile.write(newtexts)
+newfile.close 
